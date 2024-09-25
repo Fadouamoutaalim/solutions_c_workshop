8ff7a3d19796eb3e05ed32b32ec31b592f8db5cc
fadwa <example@gmail.com>
2024-09-25 12:01:20 +0200

    firstchallenge

---
+++ variables/challenge01.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    char nom [60];
+    char prenom [60];
+    char age ;
+    char sexe [60];
+    char email [60];
+    
+    printf("veuillez saisir votre nom:");
+    scanf("%s",&nom);
+    
+    printf("veuillez saisir votre prenom:");
+    scanf("%s",&prenom);
+    
+    printf("veuillez saisir votre age:");
+    scanf("%d",&age);
+    
+    printf("veuillez saisir votre sexe:");
+    scanf("%s",&sexe);
+    
+    printf("veuillez saisir votre email:");
+    scanf("%s",&email);
+    
+    
+        return 0;
+
+}
