6c3059a92b82de18285a71444bd2ff7a871cc2ca
fadwa <example@gmail.com>
2024-09-25 16:38:02 +0200

    10challenge

---
+++ challenge10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h> 
+int main(){
+     float volume , r , pi ;
+     printf("saisir r:");
+     scanf("%f",&r);
+     
+     volume = (4/3) * pi * pow(r,3);
+
+printf("le volume est:%0.2f");
+ return 0;
+}
+
+
+
+
+
+
+
+
+
+
