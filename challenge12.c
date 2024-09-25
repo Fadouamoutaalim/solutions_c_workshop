4f92d621b54eaf20112378b72f304cc3a20347d9
fadwa <example@gmail.com>
2024-09-25 17:38:04 +0200

    12challenge

---
+++ challenge12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h> 
+int main(){
+ int a ,a1 , a2 , a3 ;
+ printf("saisir un nombre a quatre chiffre a:");
+ scanf("%d",&a);
+  printf("saisir un nombre a quatre chiffre a1:");
+ scanf("%d",&a1);
+ printf("saisir un nombre a quatre chiffre a2:");
+ scanf("%d",&a2);
+  printf("saisir un nombre a quatre chiffre a3:");
+ scanf("%d",&a3);
+
+ printf("l ordre inverse de ce chiffre est:¨%d %d %d %d",a3,a2,a1,a);
+ return 0;
+}
