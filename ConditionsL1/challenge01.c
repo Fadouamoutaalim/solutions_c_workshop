a64e45c007b929818320722920820d278a6477a6
fadwa <example@gmail.com>
2024-09-26 13:03:29 +0200

    challenge1c

---
+++ ConditionsL1/challenge01.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+   int n ;
+  printf("saisir le nombre: ");
+  scanf("%d", &n);
+  if (n % 2 == 0)
+   printf("%d est un nombre pair",n);
+  else
+   printf("%d est un nombre impair",n);
+   
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
