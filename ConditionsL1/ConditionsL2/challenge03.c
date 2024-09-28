f6a8b7357e652ba03262e91741f7378d8217ad19
fadwa <example@gmail.com>
2024-09-28 12:37:47 +0200

    challenge1

--- ConditionsL1/ConditionsL2/challenge03.c
+++ ConditionsL1/ConditionsL2/challenge03.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int joursacordes, joursutilises, statut;
-    int joursrestants;
-
-    printf("Jours de conges accordes : ");
-    scanf("%d", &joursacordes);
-    printf("Jours de conges utilises : ");
-    scanf("%d", &joursutilises);
-    printf("Statut (0- pour temps partiel, 1- pour temps plein) : ");
-    scanf("%d", &statut);
-
-    joursrestants = (statut == 1) ? (joursacordes - joursutilises) : (joursacordes / 2 - joursutilises);
-
-    if (joursutilises > joursacordes) {
-        printf("Alerte : Vous avez depasse vos jours de conges !\n");
-    } else {
-        printf("Jours de conge restants : %d\n", joursrestants);
-    }
+   int m , i ;
+   for(i=1;i<=10;i++) {
+    m=4*i;
+    printf("4 * %d = %d \n ",i,m);
+   }
 
     return 0;
 }
